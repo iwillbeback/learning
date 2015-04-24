bcab887c6bf3e2df1c1b4f24978a63ba3d26f8c8
coladou <coladou@qq.com>
2015-04-24 21:07:52 +0800

    编码文件读取

--- apps/training/codec.py
+++ apps/training/codec.py
@@ -8,7 +8,6 @@ Created on 2015年4月24日
 
 import codecs
 import os
-from win32con import FILE_NAME_NORMALIZED
 
 
 def get_data_file(file_name):
@@ -21,34 +20,43 @@ def get_data_file(file_name):
 
 def read_text():
     '''
-    @summary: 用open(), 直接读取有BOM的utf-8格式文件. readline() 是字节串
+    @summary: 用open(), 直接读取有BOM的utf-8格式文件. readline() 是字节串.
+    @summary: 且需要去除BOM
     '''
     file_name = get_data_file("utf-8.csv")
     with open(file_name, "r") as fs:
-        line = fs.readline()
-        print "#line source:%s\r\n type=%s" % (line, type(line))
-
-        line_unicode = line.decode("gbk")  # line.decode("utf-8") 出错
-        print "#line unicod:%s\r\n type=%s" % (line_unicode,
-                                               type(line_unicode))
+        for i in range(0, 3):
+            line = fs.readline()
+            if (i == 0) and (line[:3] == codecs.BOM_UTF8):
+                '''
+                # 去除BOM
+                '''
+                line = line[3:]
+            line_unicode = line.decode("utf-8")
+            # print "source:%s, type=%s" % (line, type(line))
+            print "decode:%s, type=%s" % (line_unicode, type(line_unicode))
 
 
 def read_codec():
     '''
-    @summary: 用codecs.open() 读取utf-8编码的文件. readline() 是unicode 串
-    @attention: 对于有BOM 的utf-8编码的文件， 对于第一行文件。需要忽略第1个字符
+    @summary: 用codecs.open()读取文件需要指定编码,  readline() 是unicode字符串
 
     '''
-    filename = get_data_file("utf-8.csv")
-    with codecs.open(filename=filename, mode="r", encoding="utf-8",
-                     errors="strict", buffering=1) as fs:
-        line = fs.readline()
-        # print "source:%s, type=%s" % (line, type(line)) 出异常
-        print "source:%s, type=%s" % (line[1:], type(line))
 
-    filename = get_data_file('utf-8-nobom.csv')
+    extra = {"ansi.csv": "gbk",
+             "utf-8-nobom.csv": "utf-8",
+             "unicode_be.csv": "utf-16",
+             "unicode_le.csv": "utf-16"
+             }
 
-    with codecs.open(filename, mode="rb", encoding="utf-8",
-                     errors="strict", buffering=1) as fs:
-        line = fs.readline()
-        print "source:%s, type=%s" % (line, type(line))
+    for key in extra:
+        filename = get_data_file(key)
+        encoding = extra[key]
+        print "\r\n##File:", filename
+        with codecs.open(filename, mode="rb", encoding=encoding,
+                         errors="strict", buffering=1) as fs:
+            for i in range(0, 5):
+                if i == 0:
+                    pass
+                line = fs.readline()
+                print "source:%s, type=%s" % (line, type(line))
